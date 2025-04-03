9a47ed0e1d77dd4e815c40747593b5a9fb16c888
k5911839 <k5911839@gmail.com>
2025-04-04 00:54:04 +0900

    Revert "Merge pull request #1 from k59118/backend_dh"
    This reverts commit 0577934724eed4314f26fa730a36c655e1c8781f, reversing
    changes made to 6a72a67447610c5b6f6050d1db9b5b7e7822c257.
    cancel

--- backend/src/agent/state.py
+++
@@ -1,9 +0,0 @@
-from typing import Annotated, Any, List
-from langgraph.graph.message import add_messages
-from typing_extensions import TypedDict
-
-class AgentState(TypedDict):
-    """Define the state structure for the agent workflows"""
-    messages: Annotated[List, add_messages]
-    file_obj: Any  # For file uploads
-    error: str
